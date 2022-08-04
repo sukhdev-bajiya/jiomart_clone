a48d44e76ad69544c9a97f7920a3ec1438b7d060
ayush-kr05 <ayushkumar.barbighaiya@gmail.com>
2022-08-04 14:32:22 +0530

    Account Section PopUp Created

---
+++ assets/js/account.js
@@ -0,0 +1,8 @@
+function ClosePopBtn_address() {
+  document.getElementById("UpdateAddress_PopUp_section").style.display = "none";
+}
+
+function ShowChangeAddressPop() {
+  document.getElementById("UpdateAddress_PopUp_section").style.display =
+    "block";
+}
